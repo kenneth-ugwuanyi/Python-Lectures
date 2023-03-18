7ee0515d2dc7d94fd71e36801d637aeb903ef7ea
Kenneth <121946092+kenneth-ugwuanyi@users.noreply.github.com>
2023-03-18 14:16:57 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
-def login():
-    trial = 0
-    while True:
-        user_name = input("Please enter your User Name: ")
-        password = input("Please enter your Password: ")
-        trial += 1
-        if trial > 2:
-            print(f"security violation! you have exceeded {trial} trials allowed, please contact the system admin")
-        elif user_name == "Kenneth" and password == "Enter":
-            print("Welcome to UBA Payroll system")
-            break
+# import lib
+import datetime
+
+print("Attendance Register: Please ensure you sign in and out while entering and leaving the office.")
+print()
+while True:
+    time_stamp = input("Enter 1 to clock in, 2 to clock out, 3 to view hours spent at work and 4 to exit: ")
+# specify time as now or system time
+    now = datetime.datetime.now()
+# lock time to start and finish by rendering sections redundant
+    start_time = now.replace(hour=8, minute=0, second=0, microsecond=0)
+    close_time = now.replace(hour=18, minute=0, second=0,microsecond=0)
+# Note that you can nest if statements
+    if time_stamp == "1":
+        if now <= start_time:
+            print("Welcome to work! Have a productive day.")
         else:
-            print(f"invalid log in details, you have {3 -trial} trial left ")
-login()
+            print("You are late to work. Expect a query.")
+    elif time_stamp == "2":
+        if now >= close_time:
+            print("Goodbye! I hope you had a productive day.")
+        elif now <= close_time >= start_time:
+            print("You have successfully clocked out.")
+        else:
+            print("Sorry, you can't clock out. It's not closing time yet.")
+    elif time_stamp == "3":
+        if now >= start_time <= close_time:
+            print(f"You have spent {now - start_time} hours at work today.")
+        else:
+            print("Sorry, you can't view hours. It's not within work hours yet.")
+    elif time_stamp == "4":
+        break
+    else:
+        print("Invalid input.")
+
+
 
