d6126a1acaf8f1363862b1a1321626c17c5f5136
Kenneth <121946092+kenneth-ugwuanyi@users.noreply.github.com>
2023-03-18 14:19:19 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,34 +1,64 @@
-from datetime import datetime
+"""print("Welcome to the ultimate guessing game that will test your intelligence")
 
-print("WELCOME TO YOUR CLOCK-IN DEVICE")
+print()
 
-clockIn = input("Enter 'arrive' or 'leave': ").lower()  # lowercase input to avoid errors
+import random
 
-if clockIn == "arrive":
-    # set arrival time to 8:00 AM
-    arrive = datetime.now().replace(hour=8, minute=0, second=0, microsecond=0)
+correct_num = random.randint(1, 100)
 
-    # get current time
-    current_time = datetime.now().strftime("%H:%M:%S")
+trial = 5
 
-    # check if user arrived on time or not
-    if datetime.now() <= arrive:
-        print(f"Welcome to work! You arrived at {current_time} today, keep it up!")
+def correct_guess():
+     correct_guess = int(input("please input a random number"))
+
+while trial > 0:
+    select = int(input(" Please enter a random number between 1 and 100: "))
+    if select == myNumber:
+        print(" You have won the jackpot! Congratulations")
+        break
+
     else:
-        print("You are late and a penalty of GHC5.00 will apply.")
+        print("Your guess is wrong try again")
+        trial -= 1
+        print(f"You have {trial} trials left")
 
-else:
-    # set closing time to 10:00 AM
-    leave = datetime.now().replace(hour=10, minute=0, second=0, microsecond=0)
+        if trial == 0:
+            print("The game is over you have exhausted your trials! please try again later")"""
 
-    # get user's clock-out time
-    closureTime = datetime.now().strftime("%H:%M:%S")
+import random
 
-    # check if user is leaving on time or not
-    if datetime.now() >= leave:
-        print("I hope you had a good day at work today! See you tomorrow.")
-    else:
-        print(f"Sorry, it's too early to close! Our closing time is {leave}.")
+print("Welcome to the ultimate number guessing game")
+print("You have exactly five chances to win big in this game")
+
+while True:
+    right_number = random.randint(1, 100)
+    trial = 0
+
+    while trial < 5:
+        guess = int(input("Please enter a random number between 1 and 100: "))
+
+        if guess == right_number:
+            print("Congratulations! You just hit the jackpot!")
+            break
+
+        elif guess < right_number:
+            print(f"Sorry, your guess is less than the right number. You have {4 - trial} tries left.")
+            trial += 1
+
+        elif guess > right_number:
+            print(f"Sorry, your guess is higher than the right number. You have {4 - trial} trials left.")
+            trial += 1
+
+        if trial >= 5:
+            print(f"Game over! You have used the maximum of {trial} allowed. The correct answer is {right_number}.")
+            play_again = input("Enter y to start a new game, or any other key to exit: ")
+            if play_again != "y":
+                break
+                if play_again != "y":
+                    break
+
+
+
 
 
 
