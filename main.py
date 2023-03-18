088d78098d06794ef849cdef942ecf7b2a6312d8
Kenneth <121946092+kenneth-ugwuanyi@users.noreply.github.com>
2023-03-18 14:17:52 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,37 +1,54 @@
-# import lib
-import datetime
 
-print("Attendance Register: Please ensure you sign in and out while entering and leaving the office.")
-print()
-while True:
-    time_stamp = input("Enter 1 to clock in, 2 to clock out, 3 to view hours spent at work and 4 to exit: ")
-# specify time as now or system time
-    now = datetime.datetime.now()
-# lock time to start and finish by rendering sections redundant
-    start_time = now.replace(hour=8, minute=0, second=0, microsecond=0)
-    close_time = now.replace(hour=18, minute=0, second=0,microsecond=0)
-# Note that you can nest if statements
-    if time_stamp == "1":
-        if now <= start_time:
-            print("Welcome to work! Have a productive day.")
-        else:
-            print("You are late to work. Expect a query.")
-    elif time_stamp == "2":
-        if now >= close_time:
-            print("Goodbye! I hope you had a productive day.")
-        elif now <= close_time >= start_time:
-            print("You have successfully clocked out.")
-        else:
-            print("Sorry, you can't clock out. It's not closing time yet.")
-    elif time_stamp == "3":
-        if now >= start_time <= close_time:
-            print(f"You have spent {now - start_time} hours at work today.")
-        else:
-            print("Sorry, you can't view hours. It's not within work hours yet.")
-    elif time_stamp == "4":
-        break
+# Prompting the user to enter the number of courses taken
+stud_course = int(input("Please enter the number of course:? "))
+
+# Prompting the user to enter their score for a course
+score = int(input("Enter your score >>> "))
+
+# Looping through each course taken
+for i in range(stud_course):
+
+    # Assigning a letter grade and grade point based on the score
+    if score >= 80:
+        letter_grade = "A"
+        grade_point = 4.0
+    elif score >= 60:
+        letter_grade = "B"
+        grade_point = 3.0
     else:
-        print("Invalid input.")
+        letter_grade = "C"
+        grade_point = 2.0
+
+    # Printing the letter grade and grade point to the console
+    print(f"You had grade point {grade_point} and letter grade as {letter_grade}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
